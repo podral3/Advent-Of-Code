38f99365b300fd66f210650a85d899bc73eec454
podral3 <65030560+podral3@users.noreply.github.com>
2024-12-16 10:46:10 +0100

    Day 11

---
+++ 2024/Day11/Day11_1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2024.Day11
+{
+    public class Day11_1
+    {
+        Dictionary<string, List<string>> dict = new Dictionary<string, List<string>>();
+        Dictionary<long, long[]> longDict = new Dictionary<long, long[]>();
+        public long Run()
+        {
+            
+            Stopwatch stopwatch = new Stopwatch();
+            dict.Add("0", new List<string>() { "1" });
+
+            // Start measuring time
+            stopwatch.Start();
+            List<string> input = "41078 18 7 0 4785508 535256 8154 447".Split(' ').ToList();
+            //input = new List<string>() {"125", "17"};
+            List<long> list = input.Select(e => long.Parse(e)).ToList();
+            List<long> output = MyRecursion(list, 1);
+            stopwatch.Stop();
+
+            long elapsedMilliseconds = stopwatch.ElapsedMilliseconds;
+
+            // Print the elapsed time in milliseconds
+            Console.WriteLine("Execution Time: " + elapsedMilliseconds + " ms");
+            return output.Count;
+        }
+
+        private List<long> MyRecursion(List<long> list, int iteration)
+        {
+            if (iteration == 26)
+                return list;
+            else
+            {
+                List<long> newList = new List<long>();
+                foreach (long item in list)
+                {
+                    if(HandleStone1(item, out long result))
+                    {
+                        newList.Add(result);
+                    }
+                    else
+                    {
+                        var x = DivideLong(item);
+                        newList.AddRange(new List<long>() { x.Item1, x.Item2});
+                    }
+                }
+                return MyRecursion(newList, iteration+1);
+            }
+        }
+        private bool HandleStone1(long number, out long result)
+        {
+            if (number == 0) { result = 1; return true; }
+            if(number == 1) { result = 2024; return true; }
+            int digitCount = CountDigits(number);
+            if((digitCount % 2 != 0) || number > 0 && number < 10)
+            {
+                result = number * 2024; return true;
+            }
+            result = -1;
+            return false;
+        }
+        private List<string> HandleStone(string stone)
+        {
+            long stoneIne = long.Parse(stone);
+            if (stoneIne == 0)
+            {
+                return dict[stone];
+            }
+            else if (stone == "1") return new List<string>() { "2024" };
+            else if ((stone.Length % 2 != 0) || stoneIne > 0 && stoneIne < 10)
+            {
+                string stone2024String = (stoneIne * 2024).ToString();
+                return new List<string>() { stone2024String };
+            }
+            else
+            {
+                return Divide(stone);
+            }
+
+        }
+        private List<string> Divide(string input)
+        {
+            if (this.dict.ContainsKey(input))
+            {
+                return this.dict[input];
+            }
+            int mid = input.Length / 2;
+            string firstPart = input.Substring(0, mid);
+            string secondPart = input.Substring(mid);
+            var result = new List<string>() { int.Parse(firstPart).ToString(), int.Parse(secondPart).ToString() };
+            dict.Add(input, result);
+            return result;
+        }
+
+        private (long,long) DivideLong(long number)
+        {
+            long numDigits = (int)Math.Floor(Math.Log10(number) + 1);
+
+            // Step 2: Calculate the split point
+            long splitPoint = numDigits / 2;
+
+            // Step 3: Calculate the divisor for splitting
+            long divisor = (long)Math.Pow(10, numDigits - splitPoint);
+
+            // Step 4: Get the two parts
+            long part1 = number / divisor; // First part
+            long part2 = number % divisor; // Second part
+
+            return (part1, part2);
+        }
+        private int CountDigits(long num)
+        {
+            // Handle the case for zero
+            if (num == 0) return 1;
+
+            // Handle negative numbers
+            num = Math.Abs(num);
+
+            int count = 0;
+            while (num > 0)
+            {
+                num /= 10; // Divide by 10
+                count++;   // Increment count
+            }
+
+            return count;
+        }
+    }
+}
---
+++ 2024/Day11/Day11_2.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2024.Day11
+{
+    public class Day11_2
+    {
+        Dictionary<long,long> dict = new Dictionary<long,long>();
+        public long Run()
+        {
+            long count = 0;
+            List<string> input = "41078 18 7 0 4785508 535256 8154 447".Split(' ').ToList();
+            //input = new List<string>() {"125", "17"};
+            List<long> list = input.Select(e => long.Parse(e)).ToList();
+            Dictionary<long, long> numberCounts = input
+            .Select(num => long.Parse(num)) // Convert each string to long
+            .GroupBy(num => num) // Group by each number
+            .ToDictionary(group => group.Key, group => (long)group.Count());
+            //foreach (long num in list)
+            //{
+            //    if (HandleStone1(num))
+            //    {
+            //        continue;
+            //    }
+            //    else
+            //    {
+            //        DivideLong(num);
+            //    }
+            //}
+            var dic = MyRecursion(numberCounts, 1);
+            foreach(var kvp in dic)
+            {
+                count += kvp.Value;
+            }
+            return count;
+        }
+
+        private bool HandleStone1(long number, out long result)
+        {
+            result = -1; 
+            if (number == 0) { result = 1; }
+            if (number == 1) { result = 2024; }
+            int digitCount = CountDigits(number);
+            if ((digitCount % 2 != 0) || number > 0 && number < 10)
+            {
+                result = number * 2024;
+            }
+            if (result == -1) 
+                return false;
+            return true;
+        }
+        private Dictionary<long,long> MyRecursion(Dictionary<long,long>dic, int iteration)
+        {
+            if (iteration == 76)
+                return dic;
+            else
+            {
+                var shallowCopy = new Dictionary<long, long>();
+                foreach (var item in dic)
+                {
+                    if (HandleStone1(item.Key, out long result))
+                    {
+                        if (shallowCopy.ContainsKey(result))
+                        {
+                            shallowCopy[result] += dic[item.Key];
+                        }
+                        else
+                        {
+                            shallowCopy.Add(result, dic[item.Key]);
+                        }
+                    }
+                    else
+                    {
+                        DivideLong(item.Key, out long part1, out long part2);
+                        if (shallowCopy.ContainsKey(part1))
+                        {
+                            shallowCopy[part1] += dic[item.Key];
+                        }
+                        else
+                        {
+                            shallowCopy.Add(part1, dic[item.Key]);
+                        }
+                        if (shallowCopy.ContainsKey(part2))
+                        {
+                            shallowCopy[part2] += dic[item.Key];
+                        }
+                        else
+                        {
+                            shallowCopy.Add(part2, dic[item.Key]);
+                        }
+                    }
+                }
+                return MyRecursion(shallowCopy, iteration + 1);
+            }
+        }
+        
+        private void DivideLong(long number, out long part1, out long part2)
+        {
+            long numDigits = (int)Math.Floor(Math.Log10(number) + 1);
+
+            // Step 2: Calculate the split point
+            long splitPoint = numDigits / 2;
+
+            // Step 3: Calculate the divisor for splitting
+            long divisor = (long)Math.Pow(10, numDigits - splitPoint);
+
+            // Step 4: Get the two parts
+            part1 = number / divisor; // First part
+            part2 = number % divisor; // Second part
+
+            
+        }
+        private short CountDigits(long num)
+        {
+            short count = 0;
+            while (num > 0)
+            {
+                num /= 10; // Divide by 10
+                count++;   // Increment count
+            }
+
+            return count;
+        }
+    }
+}
